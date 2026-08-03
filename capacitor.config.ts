f23f5e59554f215683e658756cd29c91bbd21ffe
dono44 <don00765@gmail.com>
2026-08-03 04:20:47 +0200

    'Add'

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.djdarko.darko',
+  appName: 'djdarko44',
+  webDir: 'dist'
+};
+
+export default config;
